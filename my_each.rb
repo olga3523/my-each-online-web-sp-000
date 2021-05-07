4708b8d20f110d5e52e764bb0294cb9a6e2571f8
olga gomelskiy <olichka23.85@gmail.com>
2021-05-07 18:29:57 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
-  # code here
+
+def my_each(array)
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i += 1
+  end
+  return array
 end
+
+# The "while" method will always return a value of => nil"
+
+
+# put argument(s) here
+  # code here
